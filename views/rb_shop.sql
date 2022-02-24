9a52f3ccb68760a444d7d52b9add5d6291af8789
ridha-boughediri <ridha.boughediri@laplateforme.io>
2022-02-24 17:23:41 +0100

    adddatabasemysql

--- views/rb_shop.sql
+++ views/rb_shop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : dim. 20 fév. 2022 à 16:46
--- Version du serveur : 10.4.22-MariaDB
--- Version de PHP : 7.4.27
+-- Généré le : jeu. 24 fév. 2022 à 16:20
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,13 +37,11 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id_categorie`, `nom_categorie`) VALUES
-(16, 'captain salvador'),
-(17, 'captain tfoyu'),
-(18, 'captain america'),
-(19, 'warrior'),
-(23, 'Tongs pour femme'),
-(26, 'claquette havana'),
-(30, 'Tongs pour Hommes');
+(23, 'captain cuba'),
+(26, 'miami tongs'),
+(28, 'Tongs pour Hommes et femme'),
+(29, 'claquette havana'),
+(30, 'tong hmed');
 
 -- --------------------------------------------------------
 
@@ -64,8 +62,7 @@ INSERT INTO `couleur` (`id_couleur`, `nom_couleur`) VALUES
 (1, 'blue'),
 (2, 'blanc'),
 (3, 'rouge'),
-(4, 'jaune motarde'),
-(6, 'white black');
+(4, 'jaune laplateforme');
 
 -- --------------------------------------------------------
 
@@ -76,22 +73,44 @@ INSERT INTO `couleur` (`id_couleur`, `nom_couleur`) VALUES
 CREATE TABLE `produit` (
   `id_produit` int(11) NOT NULL,
   `nom_produit` varchar(222) NOT NULL,
-  `description` varchar(500) NOT NULL,
+  `description_produit` varchar(600) NOT NULL,
   `prix_produit` int(11) NOT NULL,
   `id_categorie` int(11) NOT NULL,
   `id_sous_catégorie` int(11) NOT NULL,
   `id_couleur` int(11) NOT NULL,
-  `images` varchar(255) NOT NULL
+  `id_produit_type` int(11) NOT NULL,
+  `images` varchar(255) NOT NULL,
+  `qte_stock` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`id_produit`, `nom_produit`, `description`, `prix_produit`, `id_categorie`, `id_sous_catégorie`, `id_couleur`, `images`) VALUES
-(13, 'Havaianas Brasil Fresh\r\n', 'Les classiques Havaianas Brasil prennent une touche de modernité. En plus du drapeau emblématique sur les lanières, ce modèle est désormais doté d\'un appelant motif dégradé en forme de diamant sur les semelles, ce qui le rend encore plus élégant.', 23, 26, 9, 4, '/images/havana_A.jpg'),
-(14, 'Havaianas Sunny II\r\n', 'Les Havaianas Sunny II sont vos sandales à porter toute la journée. Les lanières à la cheville vous offrent confort et sécurité, tout en ajoutant un petit détail à votre style. Avec une large gamme de couleurs au choix, vous en trouverez une qui vous convient… ou peut-être plusieurs.', 5, 30, 12, 4, '/images/Havaianas_homme.jpg'),
-(15, 'dqdqd', 'qdqd', 22, 30, 18, 1, '/images/castelennetong.php');
+INSERT INTO `produit` (`id_produit`, `nom_produit`, `description_produit`, `prix_produit`, `id_categorie`, `id_sous_catégorie`, `id_couleur`, `id_produit_type`, `images`, `qte_stock`) VALUES
+(14, 'produit ', 'test', 12, 23, 11, 4, 1, 'test', '100'),
+(15, '2iemtest', 'testtwo', 4, 30, 13, 3, 2, '/image/test.png', '120'),
+(16, 'test3', 'testthree', 5, 30, 11, 1, 1, '/images/test3.png', '34');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit_type`
+--
+
+CREATE TABLE `produit_type` (
+  `id_produit_type` int(11) NOT NULL,
+  `nom_produit_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `produit_type`
+--
+
+INSERT INTO `produit_type` (`id_produit_type`, `nom_produit_type`) VALUES
+(1, 'homme'),
+(2, 'femme'),
+(3, 'enfant');
 
 -- --------------------------------------------------------
 
@@ -110,36 +129,32 @@ CREATE TABLE `sous_catégorie` (
 --
 
 INSERT INTO `sous_catégorie` (`id_sous_catégorie`, `id_categorie`, `nom_sous_catégorie`) VALUES
-(9, 26, 'havana street claquette mother'),
-(12, 30, 'Havaianas Sunny'),
-(15, 30, 'havana street '),
-(18, 30, 'street czstellane ');
+(9, 29, 'havana street '),
+(10, 29, 'california street'),
+(11, 26, 'miami street homme'),
+(12, 26, 'miami  street women'),
+(13, 30, 'claquette ');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Structure de la table `utilisateurs`
 --
 
-CREATE TABLE `users` (
-  `id_users` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `prenom` varchar(250) NOT NULL,
   `adresse` varchar(255) NOT NULL,
-  `mot_passe` varchar(255) NOT NULL
+  `avatar` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `user_droit`
+-- Déchargement des données de la table `utilisateurs`
 --
 
-CREATE TABLE `user_droit` (
-  `id_user_droit` int(11) NOT NULL,
-  `id_users` int(11) NOT NULL,
-  `nom_droit` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `adresse`, `avatar`) VALUES
+(1, 'admin', 'admin', 'admin 13001', '/img/pdf.png');
 
 --
 -- Index pour les tables déchargées
@@ -164,7 +179,14 @@ ALTER TABLE `produit`
   ADD PRIMARY KEY (`id_produit`),
   ADD KEY `id_categorie` (`id_categorie`),
   ADD KEY `id_couleur` (`id_couleur`),
-  ADD KEY `id_sous_catégorie` (`id_sous_catégorie`);
+  ADD KEY `id_sous_catégorie` (`id_sous_catégorie`),
+  ADD KEY `id_produit_type` (`id_produit_type`);
+
+--
+-- Index pour la table `produit_type`
+--
+ALTER TABLE `produit_type`
+  ADD PRIMARY KEY (`id_produit_type`);
 
 --
 -- Index pour la table `sous_catégorie`
@@ -174,17 +196,10 @@ ALTER TABLE `sous_catégorie`
   ADD KEY `id_categorie` (`id_categorie`);
 
 --
--- Index pour la table `users`
+-- Index pour la table `utilisateurs`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_users`);
-
---
--- Index pour la table `user_droit`
---
-ALTER TABLE `user_droit`
-  ADD PRIMARY KEY (`id_user_droit`),
-  ADD UNIQUE KEY `id_users` (`id_users`);
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -194,37 +209,37 @@ ALTER TABLE `user_droit`
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT pour la table `couleur`
 --
 ALTER TABLE `couleur`
-  MODIFY `id_couleur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_couleur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `produit`
 --
 ALTER TABLE `produit`
-  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT pour la table `produit_type`
+--
+ALTER TABLE `produit_type`
+  MODIFY `id_produit_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `sous_catégorie`
 --
 ALTER TABLE `sous_catégorie`
-  MODIFY `id_sous_catégorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_sous_catégorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT pour la table `users`
+-- AUTO_INCREMENT pour la table `utilisateurs`
 --
-ALTER TABLE `users`
-  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `user_droit`
---
-ALTER TABLE `user_droit`
-  MODIFY `id_user_droit` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Contraintes pour les tables déchargées
@@ -236,19 +251,14 @@ ALTER TABLE `user_droit`
 ALTER TABLE `produit`
   ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `produit_ibfk_2` FOREIGN KEY (`id_couleur`) REFERENCES `couleur` (`id_couleur`),
-  ADD CONSTRAINT `produit_ibfk_3` FOREIGN KEY (`id_sous_catégorie`) REFERENCES `sous_catégorie` (`id_sous_catégorie`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `produit_ibfk_3` FOREIGN KEY (`id_sous_catégorie`) REFERENCES `sous_catégorie` (`id_sous_catégorie`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `produit_ibfk_4` FOREIGN KEY (`id_produit_type`) REFERENCES `produit_type` (`id_produit_type`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `sous_catégorie`
 --
 ALTER TABLE `sous_catégorie`
   ADD CONSTRAINT `sous_catégorie_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
---
--- Contraintes pour la table `user_droit`
---
-ALTER TABLE `user_droit`
-  ADD CONSTRAINT `user_droit_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id_users`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
