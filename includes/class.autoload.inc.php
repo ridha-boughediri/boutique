95b72f7eaa5201414126d6da32afb4006ded5b6f
ridha-boughediri <ridha.boughediri@laplateforme.io>
2022-03-02 13:54:17 +0100

    testmergeetfusion

--- includes/class.autoload.inc.php
+++ includes/class.autoload.inc.php
@@ -5,22 +5,18 @@ spl_autoload_register("myautoloader");
 
 
 
-<<<<<<< HEAD
-function myautoloader($className){
-    $pash='../classes/';
-=======
-function autoloadhtmlcss($className){
-    $pash='./classes/';
->>>>>>> 56972e4d03d1447fabecca6c69a4db2fd3662ab9
-    $ex='.class.php';
-    $fileName= $pash.$className.$ex;
-    $pathFileName=strtolower($fileName);
 
-    if(!file_exists($pathFileName)){
+function myautoloader($className)
+{
+    $pash = '../classes/';
+    $ex = '.class.php';
+    $fileName = $pash . $className . $ex;
+    $pathFileName = strtolower($fileName);
+
+    if (!file_exists($pathFileName)) {
         return false;
     }
 
 
-    include_once ($pathFileName);
+    include_once($pathFileName);
 }
-
