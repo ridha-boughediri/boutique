7c748f52586ee3b7556f4599b8a4ac1803a333c0
ridha-boughediri <ridha.boughediri@laplateforme.io>
2022-04-13 13:38:43 +0200

    style css color red

--- views/js/account.js
+++ views/js/account.js
@@ -88,7 +88,7 @@ $(document).ready(function () {
 
   $(".profile").on("click", function () {
     var profile =
-      '<h1 class="title-main" style="color: burlywood; text-decoration: underline burlywood; margin-bottom: 1vh;">Profil</h1><form id="data" method="post" enctype="multipart/form-data" class="inputs-container"> <input type="text" id="firstname" class="login-input" placeholder="Prenom*" value=""><input type="text" id="lastname" class="login-input" placeholder="Nom*" value=""><input type="mail" id="mail" class="login-input" placeholder="E-mail*" value=""><input type="password" id="password" class="login-input" placeholder="Mot de passe*"><input type="number" id="phone" class="login-input" placeholder="N° de téléphone portable*"><label class="ph">Photo de Profil: </label><input type="file"  id="file" name="file" class="button-file" value=""><p>Tous les champs marqués d\'un astérisque (*) sont obligatoires.</p><p class="field"></p><button class="button-secondary submit-edit-profile">Modifier</button></form>';
+      '<h1 class="title-main" style="color: #fb373c; text-decoration: underline #fb373c; margin-bottom: 1vh;">Profil</h1><form id="data" method="post" enctype="multipart/form-data" class="inputs-container"> <input type="text" id="firstname" class="login-input" placeholder="Prenom*" value=""><input type="text" id="lastname" class="login-input" placeholder="Nom*" value=""><input type="mail" id="mail" class="login-input" placeholder="E-mail*" value=""><input type="password" id="password" class="login-input" placeholder="Mot de passe*"><input type="number" id="phone" class="login-input" placeholder="N° de téléphone portable*"><label class="ph">Photo de Profil: </label><input type="file"  id="file" name="file" class="button-file" value=""><p>Tous les champs marqués d\'un astérisque (*) sont obligatoires.</p><p class="field"></p><button class="button-secondary submit-edit-profile">Modifier</button></form>';
 
     $(".view-container").empty();
     $(".view-container").append(profile);
@@ -96,7 +96,7 @@ $(document).ready(function () {
 
   $(".order").on("click", function () {
     var order =
-      '<h1 class="title-main" style="color: burlywood; text-decoration: underline burlywood; margin-bottom: 1vh;">Vos Commandes</h1><p>Aucune Commande</p>';
+      '<h1 class="title-main" style="color: #fb373c; text-decoration: underline #fb373c; margin-bottom: 1vh;">Vos Commandes</h1><p>Aucune Commande</p>';
 
     $(".view-container").empty();
     $(".view-container").append(order);
@@ -104,14 +104,14 @@ $(document).ready(function () {
 
   $(".adress").on("click", function () {
     var adress =
-      '<h1 class="title-main" style="color: burlywood; text-decoration: underline burlywood; margin-bottom: 1vh;">Adresse</h1>';
+      '<h1 class="title-main" style="color: #fb373c; text-decoration: underline #fb373c; margin-bottom: 1vh;">Adresse</h1>';
     $(".view-container").empty();
     $(".view-container").append(adress);
   });
 
   $(".pay").on("click", function () {
     var pay =
-      '<h1 class="title-main" style="color: burlywood; text-decoration: underline burlywood; margin-bottom: 1vh;">Vos Paiement</h1>';
+      '<h1 class="title-main" style="color: #fb373c; text-decoration: underline #fb373c; margin-bottom: 1vh;">Vos Paiement</h1>';
     $(".view-container").empty();
     $(".view-container").append(pay);
   });
