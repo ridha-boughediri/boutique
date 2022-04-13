9903c11691dd71e341624e7520514b3a74497931
Samir-Belhadj <samir.belhadj-benziane@laplateforme.io>
2022-04-13 13:37:24 +0200

    margin card

--- views/search.php
+++ views/search.php
@@ -1,8 +1,9 @@
+<?php $searchprod = $monproduit->getProduitByLike($params[1]);?>
 <main>
     <div class="best-carousel">
         <h1 class="title-main">Vos resultats pour :</h1>
         <div class="slick-cards">
-            <?php foreach ($monproduit->getProduitByLike($params[1]) as $allproduit) { ?>
+            <?php foreach ($searchprod as $allproduit) { ?>
                 <div class="slick-card" data-id="<?= $allproduit["id_produit"] ?>">
                     <img src="views/img/admin/<?= $allproduit["file_images"] ?>" alt="" class="slick-img">
                     <div class="slick-card-infos">
