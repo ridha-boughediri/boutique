a20ca8a52152764d2981e2f561d00e4d5aa1f7f6
Samir Belhadj <samir.belhadj-benziane@laplateforme.io>
2022-04-19 10:47:23 +0200

    Card repair

--- views/produits.php
+++ views/produits.php
@@ -4,14 +4,6 @@
   <?php $sizeprod = count($prodbycateandsouscate); ?>
 
   <div class="container-list-produit">
-    <div class="list-container-produit scroll-bar-small">
-      <h2 class="title-produit-bar">Couleurs</h2>
-      <div class="list-btn-product">
-        <?php foreach ($couleur->getCoul() as $couleurinfos) { ?>
-          <button class="button-eight btn-filter-color" data-id="<?= $couleurinfos['id_couleur'] ?>" data-cate="<?= $params[1] . '-' . $params[2] ?>"><?= $couleurinfos['nom_couleur'] ?></button>
-        <?php } ?>
-      </div>
-    </div>
 
     <?php if ($sizeprod == 0) { ?>
       <div class="best-carousel">
@@ -23,8 +15,8 @@
       <div class="best-carousel padding-tb1">
         <div class="slick-cards">
           <?php foreach ($prodbycateandsouscate as $allproduit) { ?>
-            <div class="slick-card" data-id="<?= $allproduit["id_produit"] ?>">
-              <img src="views/img/admin/<?= $allproduit["file_images"] ?>" alt="" class="slick-img">
+            <div class="slick-card">
+              <img src="views/img/admin/<?= $allproduit["file_images"] ?>" alt="" class="slick-img" data-id="<?= $allproduit["id_produit"] ?>">
               <div class="slick-card-infos">
                 <h3><?= $allproduit["nom_produit"] ?></h3>
                 <p><?= $allproduit["prix_produit"] ?>€</p>
