d508f46fb99349e6ccc1a5aa36fc1889213fe75e
Samir Belhadj <samir.belhadj-benziane@laplateforme.io>
2022-04-25 17:03:31 +0200

    update categorie

---
+++ controllers/process_update_categorie.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+extract($_POST);
+extract($_FILES);
+
+require("../vendor/autoload.php");
+
+
+$categorie = new Categorie();
+$couleur = new Couleur();
+$database = new DataBase();
+$panier = new Panier();
+$monproduit = new Produit();
+$souscategorie = new Souscategorie();
+$type = new Type();
+$user = new User();
+
+
+
+
+
+$id_categorie= intval($_POST['id']);
+$nom_categorie= htmlspecialchars($_POST['categorie']);
+
+
+
+
+
+
+echo $categorie->UpdateCategorie($nom_categorie, $id_categorie);
